827120d233c411ef8bc16c8945598420f8eda573
nikanoza <nikanozadze1010@gmail.com>
2022-08-10 18:29:51 +0400

    feat: create auth schema

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import http from 'http'
 import express from 'express'
 import dotenv from 'dotenv'
-import bcrypt from 'bcrypt'
 import bodyParser from 'body-parser'
 
 import connectToMongo from './config/mongo.js'
@@ -14,8 +13,4 @@ connectToMongo()
 app.use(bodyParser.json())
 
 const server = http.createServer(app)
-server.listen(process.env.SERVER_PORT, async ()=> {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash('dardubala', salt)
-    console.log(hashedPassword)
-})
+server.listen(process.env.SERVER_PORT)
