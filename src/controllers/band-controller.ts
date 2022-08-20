fdb0da991f07c83563a537206a4e9e7029453a76
nikanoza <nikanozadze1010@gmail.com>
2022-08-20 13:50:48 +0400

    wip: work on create band function

--- src/controllers/band-controller.ts
+++ src/controllers/band-controller.ts
@@ -1,12 +1,6 @@
 import express from 'express'
 
-// import { IBand } from 'models/Band'
-// import { TypedRequestBody } from "types/services.d.js"
-
 export const createBand = async (req: express.Request, res: express.Response) => {
-    // const {body } = req
-    // const logo = req.file
-    // const { description, extra } = body
     console.log(req.file)
     return res.status(201).statusMessage
 }
