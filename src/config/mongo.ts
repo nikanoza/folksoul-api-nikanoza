2f08335bfe3295611c99758d348ee49188e4375f
nikanoza <nikanozadze1010@gmail.com>
2022-08-23 09:29:37 +0400

    fix: fix .env variabels was udefined and server url in swagger

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
-const { MONGO_PASSWORD, MONGO_PROTOCOL, MONGO_USER, MONGO_HOST, MONGO_PORT, MONGO_DATABASE } = process.env;
-
-const isAtlas = () => MONGO_PROTOCOL !== 'mongodb'
-const generateAtlasUrl = () => `${MONGO_PROTOCOL}://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}`
-const generateLocalUrl = () => `${MONGO_PROTOCOL}://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`
+const isAtlas = () => process.env.MONGO_PROTOCOL !== 'mongodb'
+const generateAtlasUrl = () => 
+  `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}`
+const generateLocalUrl = () => 
+  `${process.env.MONGO_PROTOCOL}://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`
 
 
 const connect = async () => {
