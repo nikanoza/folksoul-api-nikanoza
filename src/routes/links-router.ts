49223730a814bf2b204c900b103961bb5a075626
nikanoza <nikanozadze1010@gmail.com>
2022-09-03 10:00:30 +0400

    feat: create social link update route and add it to swagger

--- src/routes/links-router.ts
+++ src/routes/links-router.ts
@@ -2,9 +2,10 @@ import express from 'express'
 import { LinksController } from 'controllers'
 
 const linksRouter = express.Router()
-const { addNewLink, getSocialLink } = LinksController
+const { addNewLink, getSocialLink, updateSocialLink } = LinksController
 
 linksRouter.post('/links/new', addNewLink)
 linksRouter.get('/links/:id', getSocialLink)
+linksRouter.put('/links/edit/:id', updateSocialLink)
 
 export default linksRouter
