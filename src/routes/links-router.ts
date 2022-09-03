bc7991a3a9015f491e9c6da7e4a100c90db2f82a
nikanoza <nikanozadze1010@gmail.com>
2022-09-03 09:33:05 +0400

    feat: add get single link route and add it to swagger

--- src/routes/links-router.ts
+++ src/routes/links-router.ts
@@ -2,8 +2,9 @@ import express from 'express'
 import { LinksController } from 'controllers'
 
 const linksRouter = express.Router()
-const { addNewLink } = LinksController
+const { addNewLink, getSocialLink } = LinksController
 
 linksRouter.post('/links/new', addNewLink)
+linksRouter.get('/links/:id', getSocialLink)
 
 export default linksRouter
